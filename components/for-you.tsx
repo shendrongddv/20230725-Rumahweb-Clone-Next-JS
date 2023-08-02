03bab5645e97c65aa87f41311df20e4f9d0c7b2f
shendrongddv <shendrongddv@gmail.com>
2023-08-02 21:45:03 +0700

    Rabu, 2 Agustus 2023

--- components/for-you.tsx
+++ components/for-you.tsx
@@ -1,17 +1,6 @@
 import Image from "next/image";
 
 import { LucideIcon, Monitor } from "lucide-react";
-import { Button } from "@/components/ui/button";
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardFooter,
-  CardHeader,
-  CardTitle,
-} from "@/components/ui/card";
-import { Input } from "@/components/ui/input";
-import { Label } from "@/components/ui/label";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 
 // Content
